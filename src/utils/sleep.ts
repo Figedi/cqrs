3c0aa22733fe5c3ceb3e9f732da32a4c998f4254
Felix Kaminski <kaminski.felix@gmail.com>
2026-01-31 11:54:12 +0100

    fix: ci config fixes, retriggers pipeline

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -2,6 +2,6 @@ export const sleep = (timeMs: number, unref?: boolean): Promise<void> =>
   new Promise(resolve => {
     const timeout = setTimeout(resolve, timeMs)
     if (unref) {
-      ;(timeout as any).unref()
+      timeout.unref()
     }
-  })
+})
